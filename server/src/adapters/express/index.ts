f6494c8e76155a0aef1defb7d9cc5d824d93a8ff
Anas R <anas-r@github.io>
2024-10-15 00:02:37 +0200

    remove boilerplate

--- server/src/adapters/express/index.ts
+++ server/src/adapters/express/index.ts
@@ -4,7 +4,6 @@ import { IRunnable, IStoppable } from '@core/types';
 import { Tag } from '@core/provider';
 import * as ProjectController from './project.adapter';
 import * as TodoController from './todo.adapter';
-import { Constructor } from '@core/types';
 import type { IncomingMessage, Server, ServerResponse } from 'http';
 import type { Application } from 'express';
 
@@ -12,7 +11,7 @@ export class AppModule implements IRunnable, IStoppable {
   private _app = Context.get(EXPRESS_APP);
   private config = Context.get(EXPRESS_CONFIG);
 
-  private _server: Server<typeof IncomingMessage, typeof ServerResponse> | null = null;
+  private server: Server<typeof IncomingMessage, typeof ServerResponse> | null = null;
 
   constructor() {
     this._app.use(bodyParser.json());
@@ -25,15 +24,15 @@ export class AppModule implements IRunnable, IStoppable {
   }
 
   async run() {
-    this._server = this._app.listen(this.config.port, () =>
+    this.server = this._app.listen(this.config.port, () =>
       console.log(`App is running on ${this.config.scheme}://${this.config.host}:${this.config.port}`)
     );
 
-    this._server.on('error', () => this._server!.close());
+    this.server.on('error', () => this.server!.close());
   }
 
   async stop() {
-    if (this._server) this._server.close();
+    if (this.server) this.server.close();
   }
 }
 
@@ -41,15 +40,6 @@ interface IExpressConfig {
   port: number;
   scheme: string;
   host: string;
-}
-
-export class ExpressConfig {
-  static of = (config: Partial<IExpressConfig>): Constructor<IExpressConfig> =>
-    class {
-      port = config.port || 3000;
-      scheme = config.scheme || 'http';
-      host = config.host || 'localhost';
-    };
 }
 
 export const EXPRESS_APP = new Tag<Application>('express/app');
